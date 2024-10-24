398b6ae637e4f8ebd0619d54e9e6910092663f19
Sergei Garin <sergei.garin@enso.org>
2024-10-24 18:41:30 +0300

    Small fixes

--- app/gui/src/dashboard/layouts/AssetDiffView/AssetDiffView.tsx
+++ app/gui/src/dashboard/layouts/AssetDiffView/AssetDiffView.tsx
@@ -7,7 +7,9 @@ import Spinner, * as spinnerModule from '#/components/Spinner'
 
 import type * as backendService from '#/services/Backend'
 import type Backend from '#/services/Backend'
-import { useFetchVersionContent } from './useFetchVersionContent'
+
+import { useSuspenseQueries } from '@tanstack/react-query'
+import { versionContentQueryOptions } from './useFetchVersionContent'
 
 // =====================
 // === AssetDiffView ===
@@ -24,17 +26,20 @@ export interface AssetDiffViewProps {
 /** Diff view comparing `Main.enso` of two versions for a specific project. */
 export function AssetDiffView(props: AssetDiffViewProps) {
   const { versionId, project, backend, latestVersionId } = props
-  const { getText } = textProvider.useText()
 
-  const versionContent = useFetchVersionContent({
-    versionId,
-    project,
-    backend,
-  })
-  const headContent = useFetchVersionContent({
-    versionId: latestVersionId,
-    project,
-    backend,
+  const [versionContent, headContent] = useSuspenseQueries({
+    queries: [
+      versionContentQueryOptions({
+        versionId,
+        projectId: project.id,
+        backend,
+      }),
+      versionContentQueryOptions({
+        versionId: latestVersionId,
+        projectId: project.id,
+        backend,
+      }),
+    ],
   })
 
   const loader = (
@@ -43,30 +48,24 @@ export function AssetDiffView(props: AssetDiffViewProps) {
     </div>
   )
 
-  if (versionContent.isError || headContent.isError) {
-    return <div className="p-indent-8 text-center">{getText('loadFileError')}</div>
-  } else if (versionContent.isPending || headContent.isPending) {
-    return loader
-  } else {
-    return (
-      <monacoReact.DiffEditor
-        beforeMount={(monaco) => {
-          monaco.editor.defineTheme('myTheme', {
-            base: 'vs',
-            inherit: true,
-            rules: [],
-            // The name comes from a third-party API and cannot be changed.
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            colors: { 'editor.background': '#00000000' },
-          })
-        }}
-        original={versionContent.data}
-        modified={headContent.data}
-        language="enso"
-        options={{ readOnly: true }}
-        loading={loader}
-        theme={'myTheme'}
-      />
-    )
-  }
+  return (
+    <monacoReact.DiffEditor
+      beforeMount={(monaco) => {
+        monaco.editor.defineTheme('myTheme', {
+          base: 'vs',
+          inherit: true,
+          rules: [],
+          // The name comes from a third-party API and cannot be changed.
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          colors: { 'editor.background': '#00000000' },
+        })
+      }}
+      original={versionContent.data}
+      modified={headContent.data}
+      language="enso"
+      options={{ readOnly: true }}
+      loading={loader}
+      theme={'myTheme'}
+    />
+  )
 }
