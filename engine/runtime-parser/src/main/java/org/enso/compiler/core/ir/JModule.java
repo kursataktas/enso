df95a4e4cfa6bcd51156d4306305177273fd5e31
Pavel Marek <pavel.marek@enso.org>
2024-10-25 17:24:28 +0200

    JModule extends IR

--- engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/JModule.java
+++ engine/runtime-parser/src/main/java/org/enso/compiler/core/ir/JModule.java
@@ -1,13 +1,14 @@
 package org.enso.compiler.core.ir;
 
 import java.util.List;
+import org.enso.compiler.core.IR;
 import org.enso.compiler.core.ir.module.scope.JExport;
 import org.enso.compiler.core.ir.module.scope.JImport;
 import org.enso.runtime.parser.dsl.IRChild;
 import org.enso.runtime.parser.dsl.IRNode;
 
 @IRNode
-public interface JModule {
+public interface JModule extends IR {
   @IRChild
   List<JImport> imports();
 
