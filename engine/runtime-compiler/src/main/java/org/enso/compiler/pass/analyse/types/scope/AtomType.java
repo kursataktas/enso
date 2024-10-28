7949ed8a577e0da8f5d86f4ab899ddaaeb8fa490
Radosław Waśko <radoslaw.wasko@enso.org>
2024-10-28 18:30:27 +0100

    WIP

--- engine/runtime-compiler/src/main/java/org/enso/compiler/pass/analyse/types/scope/AtomType.java
+++ engine/runtime-compiler/src/main/java/org/enso/compiler/pass/analyse/types/scope/AtomType.java
@@ -1,5 +1,7 @@
 package org.enso.compiler.pass.analyse.types.scope;
 
+import org.enso.compiler.pass.analyse.types.TypeRepresentation;
+
 import java.util.List;
 
 public final class AtomType {
@@ -13,7 +15,15 @@ public final class AtomType {
     return name;
   }
 
-  // This is a sibling to BindingsMap.Cons. For now kept separate on purpose.
-  // TODO do we want arguments or a signature here?
-  public record Constructor(String name, boolean isProjectPrivate) {}
+  // TODO mark type in constructor only if no default arguments, treat same as function?
+  // TODO this should replace AtomTypeInterface
+
+  /**
+   * Represents a constructor of the atom type.
+   *
+   * @param name the name of the constructor
+   * @param isProjectPrivate whether the constructor is project private
+   * @param type the type ascribed to the constructor, it may be null if it is unknown
+   */
+  public record Constructor(String name, boolean isProjectPrivate, TypeRepresentation type) {}
 }
