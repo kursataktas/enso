d31d1455ff6838f5189386183121857ebde47eba
somebody1234 <ehern.lee@gmail.com>
2024-10-30 20:58:02 +1000

    Update settings form default values on submit

--- app/gui/src/dashboard/layouts/Settings/FormEntry.tsx
+++ app/gui/src/dashboard/layouts/Settings/FormEntry.tsx
@@ -1,7 +1,8 @@
 /** @file Rendering for an {@link SettingsFormEntryData}. */
 import { ButtonGroup, Form } from '#/components/AriaComponents'
+import { useSyncRef } from '#/hooks/syncRefHooks'
 import { useText } from '#/providers/TextProvider'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import SettingsInput from './Input'
 import type { SettingsContext, SettingsFormEntryData } from './data'
 
@@ -22,6 +23,7 @@ export function SettingsFormEntry<T extends Record<keyof T, string>>(
   const { context, data } = props
   const { schema: schemaRaw, getValue, inputs, onSubmit, getVisible } = data
   const { getText } = useText()
+  const [dirty, setDirty] = useState(false)
   const visible = getVisible?.(context) ?? true
   const value = getValue(context)
   const schema = useMemo(
@@ -29,15 +31,29 @@ export function SettingsFormEntry<T extends Record<keyof T, string>>(
     [context, schemaRaw],
   )
 
+  const form = Form.useForm({
+    // @ts-expect-error This is SAFE, as the type `T` is statically known.
+    schema,
+    defaultValues: value,
+    onSubmit: async (newValue) => {
+      // @ts-expect-error This is SAFE, as the type `T` is statically known.
+      await onSubmit(context, newValue)
+      setDirty(true)
+    },
+  })
+
+  const dirtyEffectDeps = useSyncRef({ context, form, getValue })
+
+  useEffect(() => {
+    const deps = dirtyEffectDeps.current
+    if (dirty) {
+      setDirty(false)
+      deps.form.reset(deps.getValue(deps.context))
+    }
+  }, [dirty, dirtyEffectDeps])
+
   return !visible ? null : (
-      <Form
-        gap="none"
-        // @ts-expect-error This is SAFE, as the type `T` is statically known.
-        schema={schema}
-        defaultValues={value}
-        // @ts-expect-error This is SAFE, as the type `T` is statically known.
-        onSubmit={(newValue) => onSubmit(context, newValue)}
-      >
+      <Form form={form} gap="none">
         {inputs.map((input) => (
           <SettingsInput key={input.name} context={context} data={input} />
         ))}
