86d1b0eedc6505e976d09c5bf387494c224349fc
Pavel Marek <pavel.marek@enso.org>
2024-10-31 18:12:10 +0100

    docs

--- engine/runtime-parser-dsl/src/main/java/org/enso/runtime/parser/dsl/IRChild.java
+++ engine/runtime-parser-dsl/src/main/java/org/enso/runtime/parser/dsl/IRChild.java
@@ -5,8 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Abstract methods annotated with this annotation will return the <emph>child</emph> of the current
+ * IR element (the current IR element is the interface annotated with {@link IRNode} that encloses
+ * this method). Children of IR elements form a tree. A child will be part of the methods traversing
+ * the tree, like {@code mapExpression} and {@code children}. The method must have no parameters and
+ * return a subtype of {@code org.enso.compiler.ir.IR}.
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
 public @interface IRChild {
+  /** If true, the child will always be non-null. Otherwise, it can be null. */
   boolean required() default true;
 }
--- engine/runtime-parser-dsl/src/main/java/org/enso/runtime/parser/dsl/IRNode.java
+++ engine/runtime-parser-dsl/src/main/java/org/enso/runtime/parser/dsl/IRNode.java
@@ -5,6 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * An interface annotated with this annotation will be processed by the IR processor. The processor
+ * will generate a class that extends this interface. The generated class will have the same package
+ * as this interface, and its name will have the "Gen" suffix. The interface must be a subtype of
+ * {@code org.enso.compiler.ir.IR}. The interface can contain {@link IRChild} and {@link
+ * IRCopyMethod} annotated methods.
+ *
+ * <p>For every abstract parameterless method of the interface, there will be a field in the
+ * generated class.
+ *
+ * <p>The interface can contain arbitrary number of nested interfaces. In such case, the processor
+ * will generate nested static classes for all these nested interfaces.
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface IRNode {}
